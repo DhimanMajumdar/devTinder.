d349abba22bc17512e869fffab1bef7c5c857302
dhiman majumdar <dhimanmajumdar08233@gmail.com>
2024-09-19 21:33:27 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@ const express=require("express");
 
 const app=express();
 
+// app.use("/route",[rh1,rh2,rh3],rh4);
 
-app.get("/user/:userId", (req,res)=>{
-    //console.log(req.query);
-    console.log(req.params);                                                                                                                                                   
-    res.send({firstName:"Dhiman","lastName":"Majumdar"});
-})
+app.use(
+    "/user",
+    (req,res,next)=>{
+        console.log("Handling the route user!!");
+        next();
+        //res.send("Response!!");
+    },
+    (req,res,next)=>{
+        console.log("Handling the route user 2!!");
+        res.send("2nd response!!");
+         
+    }
+);
 
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000...");
