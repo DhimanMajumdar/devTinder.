b104a57df01fad38a0dbe1a849788f52d079d163
dhiman majumdar <dhimanmajumdar08233@gmail.com>
2024-09-25 11:30:46 +0530

    api level data sanitization

--- src/app.js
+++ src/app.js
@@ -3,20 +3,85 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
   // Creating a new instance of the User Model
-  const user = new User({
-    firstName: "Cristiano",
-    lastName: "Ronaldo",
-    emailId: "cristiano@ronaldo.com",
-    password: "cr@123",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("User added successfully");
   } catch (err) {
     res.status(400).send("Error saving the user:" + err.message);
+  }
+});
+
+// Get user by email
+app.get("/user",async (req,res)=>{
+  const userEmail=req.body.emailId;
+
+  try{
+    const users=await User.findOne({emailId:userEmail});
+    res.send(users);
+
+    // const users=await User.find({emailId:userEmail});
+    // if(users.length===0) res.status(404).send("User not found");
+    // else res.send(users);
+  }
+  catch{
+    res.status(400).send("Something went wrong!!");
+  }
+})
+
+// Delete API - Delete an user from a database
+app.delete("/user", async (req,res)=>{
+  const userId=req.body.userId; 
+  try{
+    const users=await User.findByIdAndDelete(userId);
+    res.send(users);
+  }
+  catch{
+    res.status(400).send("id not found");
+  }
+})
+
+// Update data of the user
+app.patch("/user/:userId",async (req,res)=>{
+  const userId=req.params?.userId;
+  const data=req.body;
+ 
+  try{
+    const ALLOWED_UPDATES=["photUrl","about","gender","age","skills"];
+    const isUpdateAllowed=Object.keys(data).every((k)=>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if(!isUpdateAllowed){
+      throw new Error("Update not allowed");
+    }
+    if(data?.skills.length>10){
+      throw new Error("Skills can not be more than 10");
+    }
+    const user=await User.findByIdAndUpdate({_id:userId},data,{
+      returnDocument:"after",
+      runValidators:true,
+    });
+    console.log(user);
+    res.send("User Updated successfully");
+  }
+  catch(err){
+    res.status(400).send("something went wrong!!");
+  }
+})
+
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed",async (req,res)=>{
+  try{
+    const users=await User.find({});
+    res.send(users);
+  } catch(err){
+    res.status(400).send("Something went wrong!!");
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,48 @@ const mongoose=require("mongoose");
 
 const userSchema= new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        minLength:4,
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
     },
     password:{
-        type:String
+        type:String,
+        required:true,
     },
     age:{
-        type:Number
+        type:Number,
+        min:18,
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data is not valid!")
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://imc-armenia.org/armenian-consultants/members/full/page/3/",
+    },
+    about:{
+        type:String,
+        default:"This is a default about of the user!",
+    },
+    skills:{
+        type:[String],
     }
+},{
+    timestamps:true,
 })
 
 module.exports=mongoose.model("User",userSchema);
