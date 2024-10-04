f71b9a62ec5aa2276c81967bd0c9f29883e8b902
dhiman majumdar <dhimanmajumdar08233@gmail.com>
2024-10-04 06:45:59 +0530

    made /user/connections and corner cases

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const userRouter=express.Router();
 
+const USER_SAVE_DATA="firstName lastName photoUrl age gender aboutskills";
 // get all the pending connection request for the loggedIn user
 userRouter.get("/user/requests/received", userAuth ,async (req,res)=>{
     try{
@@ -19,5 +20,21 @@ userRouter.get("/user/requests/received", userAuth ,async (req,res)=>{
         res.status(400).send("ERROR :" + err.message);
     }
 } )
+
+userRouter.get("/user/connections", userAuth, async(req,res)=>{
+    try{
+        const loggedInUser=req.user;
+        const connectionRequests=await ConnectionRequest.find({
+            $or:[
+                {toUserId:loggedInUser._id,status:"accepted"},
+                {fromUserId:loggedInUser._id,status:"accepted"},
+            ]
+        }).populate("fromUserId",USER_SAVE_DATA);
+        const data=connectionRequests.map(row=>row.fromUserId);
+        res.json({data});
+    } catch(err){
+        res.status(400).send("ERROR :" + err.message);
+    }
+})
 
 module.exports=userRouter;
