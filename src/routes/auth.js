8e5b1c6c41f67398c65fb24e0a1d14c32742c7ef
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2024-10-19 00:43:37 +0530

    made some jwt and cookie chnages for frontend signup logic

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,29 +5,44 @@ const User=require("../models/user");
 const bcrypt=require("bcrypt");
 
 authRouter.post("/signup", async (req, res) => {
-    try {
-      // Validation of data
-      validateSignUpData(req);
-  
-      const { firstName, lastName, emailId, password } = req.body;
-  
-      // Encrypt the password
-      const passwordHash = await bcrypt.hash(password, 10);
-      console.log(passwordHash);
-      // Creating a new instance of the User Model
-      const user = new User({
-        firstName,
-        lastName,
-        emailId,
-        password: passwordHash,
-      });
-  
-      await user.save();
-      res.send("User added successfully");
-    } catch (err) {
-      res.status(400).send("ERROR : " + err.message);
+  try {
+    // Validate the input data
+    validateSignUpData(req);
+
+    const { firstName, lastName, emailId, password, gender } = req.body;
+
+    // Check if the user already exists
+    const existingUser = await User.findOne({ emailId });
+    if (existingUser) {
+      return res.status(400).send("User with this email already exists.");
     }
-  });
+
+    // Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    // Create a new instance of the User Model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      gender,
+      password: passwordHash,
+    });
+
+    const savedUser=await user.save();
+    const token=await savedUser.getJWT();
+
+    res.cookie("token",token,{
+      expires:new Date(Date.now()+8*3600000),
+    });
+
+    // Send a success response
+    res.json({message:"user Added Successfully!", data:savedUser})
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
 
 authRouter.post("/login", async (req, res) => {
     try {
